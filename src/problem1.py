b9541e2c2df17957dbe5dac7143e9def26e40371
mohammedali2299 <alima@rose-hulman.edu>
2018-05-25 11:56:20 -0400

    problem 1

--- src/problem1.py
+++ src/problem1.py
@@ -2,9 +2,9 @@
 Final exam, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and mohammed ali.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -19,8 +19,8 @@ def main():
     print()
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
-    # run_test_problem1a()
-    # run_test_problem1b()
+    #run_test_problem1a()
+    run_test_problem1b()
 
 
 def run_test_problem1a():
@@ -94,6 +94,10 @@ def problem1a(numbers):
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
+    first = numbers[0]
+    last = numbers[len(numbers)-1]
+    middle = numbers[int(len(numbers))/2]
+    return first + middle + last
 
 def run_test_problem1b():
     """ Tests the  problem1b   function. """
@@ -169,7 +173,12 @@ def problem1b(numbers):
     # TODO: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
+    rang = int(len(numbers)/3)
+    print(rang)
+    sum = 0
+    for k in range(rang):
+        sum = sum + numbers[k+rang]
+    return sum
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
